554b23dadba0393e553adc5d84ed7935300040fe
VeryBaaad <verybaaad@outlook.com>
2026-04-04 11:39:49 +0800

    fix(help): update help template to match renamed parameter

--- mmover/src/main.rs
+++ mmover/src/main.rs
@@ -14,16 +14,17 @@ use enigo::{Axis, Button, Coordinate, Enigo, Mouse, Settings};
     help_template = "\
 Usage: {name} [options]
 Options:
-  -x=[value]         Move mouse on X axis
-  -y=[value]         Move mouse on Y axis
-  --click-left       Click left button
-  --click-right      Click right button
-  --click-middle     Click middle button
-  --click-forward    Click forward side button
-  --click-back       Click back side button
-  --roll=<N> -r=<N>  Scroll wheel up by N
-  --get, -g          Get current mouse position
-  --help, -h         Show this help
+  -x=[value]            Move mouse on X axis
+  -y=[value]            Move mouse on Y axis
+  --click-left          Click left button
+  --click-right         Click right button
+  --click-middle        Click middle button
+  --click-forward       Click forward side button
+  --click-back          Click back side button
+  --vertical-roll=<N>   Vertical Scroll
+  --horizontal-roll=<N> Scroll wheel up by N
+  --get, -g             Get current mouse position
+  --help                Show this help
 "
 )]
 struct Args {
